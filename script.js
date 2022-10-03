36e95b40c972b44069e870a79191928531e2af4a
Van Vasquez <vanvasquez0725@gmail.com>
2022-10-03 22:21:53 +0800

    v2

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 let hour = 0,
-	minute = 0,
-	second = 0;
+  minute = 0,
+  second = 0;
 const milisecond = 1000;
 const limit = 60;
 let startTime;
 document.querySelector('#start').addEventListener('click', () => {
-	startTime = setInterval(timer, milisecond);
+  startTime = setInterval(timer, milisecond);
 });
 document.querySelector('#stop').addEventListener('click', () => {
-	clearInterval(startTime);
+  clearInterval(startTime);
 });
 document.querySelector('#reset').addEventListener('click', () => {
-	window.location.reload();
+  window.location.reload();
 });
 function timer() {
-	second++;
-	if (second > limit) {
-		minute++;
-		second = 0;
-	} else if (minute > limit) {
-		hour++;
-		minute = 0;
-	}
-	render();
+  second++;
+  if (second > limit) {
+    minute++;
+    second = 0;
+  } else if (minute > limit) {
+    hour++;
+    minute = 0;
+  }
+  render();
 }
 
 function render() {
-	document.querySelector('#hour').innerText = ('0' + hour).slice(-2);
-	document.querySelector('#minute').innerText = ('0' + minute).slice(-2);
-	document.querySelector('#second').innerText = ('0' + second).slice(-2);
+  document.querySelector('#hour').innerText = ('0' + hour).slice(-2);
+  document.querySelector('#minute').innerText = ('0' + minute).slice(-2);
+  document.querySelector('#second').innerText = ('0' + second).slice(-2);
 }
